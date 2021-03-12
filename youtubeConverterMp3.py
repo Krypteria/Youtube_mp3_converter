bc9f38b99ded6bc43142905251dda9d7dd6900a0
Krypteria <55555187+Krypteria@users.noreply.github.com>
2021-03-12 18:27:57 +0100

    Update youtubeConverterMp3.py

--- youtubeConverterMp3.py
+++ youtubeConverterMp3.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import filedialog
 from tkinter import messagebox
 from tkinter import ttk
+from tkinter import Menu
 import youtube_dl
 import ffmpeg
 import os
@@ -9,6 +10,8 @@ import os
 WIDTH = 725
 HEIGHT = 110
 
+# Configuración de la pantalla
+# --------------------------------------------------------------------------
 root = tk.Tk()
 root.title("Conversor de videos a MP3")
 root.config(width=WIDTH, height=HEIGHT)
@@ -16,11 +19,12 @@ root.resizable(False, False)
 
 SCREEN_WIDTH = root.winfo_screenwidth()
 SCREEN_HEIGHT = root.winfo_screenheight()
+x_coord = int((SCREEN_WIDTH/2) - (WIDTH/2))
+y_coord = int((SCREEN_HEIGHT/2) - (HEIGHT/2))
+root.geometry("{}x{}+{}+{}".format(WIDTH, HEIGHT, x_coord, y_coord))
 
-fuente = ('Helvetica', 10, 'bold')
-
-RUTA = ""
-
+# Formato de conversión
+# --------------------------------------------------------------------------
 formato = {
     'format': 'bestaudio/best',
     'postprocessors': [{
@@ -30,7 +34,9 @@ formato = {
     }],
 }
 
-## ETIQUETAS
+# Etiquetas 
+# --------------------------------------------------------------------------
+fuente = ('Helvetica', 10, 'bold')
 
 etiquetaTitulo = tk.Label(root, text="Video a convertir: ",  font=fuente)
 etiquetaTitulo.place(x = 17, y = 10)
@@ -38,20 +44,21 @@ etiquetaTitulo.place(x = 17, y = 10)
 etiquetaEstado = tk.Label(root, text="Estado de la conversión: ", font=fuente)
 etiquetaEstado.place(x = 17, y = 70)
 
-## CUADROS DE TEXTO
+# Input 
+# --------------------------------------------------------------------------
+imagenCambiarRuta = tk.PhotoImage(file = "D:\Biblioteca\Escritorio\Workspaces\Workspace Python\Tests\cambiarRuta.png") #cambiar
+RUTA = ""
 
+#Cuadro de texto
 entrada = ttk.Entry(root, width=90)
 entrada.place(x=20, y=40)
 
-## VARIABLES
+#Etiqueta de estado
 estado = tk.StringVar()
 estadoContenedor = tk.Label(root, textvariable=estado,  font=fuente)
 estadoContenedor.place(x = 190, y = 70)
 
-## BOTONES
-
-imagenCambiarRuta = tk.PhotoImage(file = "D:\Biblioteca\Escritorio\Workspaces\Workspace Python\Tests\cambiarRuta.png")
-
+#Botones
 def convertir():
     if(RUTA == ""):
         tk.messagebox.showinfo("Error", "Tienes que seleccionar una carpeta de destino")
@@ -65,8 +72,8 @@ def convertir():
             estado.set("Completada")
 
 
-convertirBotton = ttk.Button(root, text="Convertir", command = convertir)
-convertirBotton.place(x=575, y=38)
+convertirBoton = ttk.Button(root, text="Convertir", command = convertir)
+convertirBoton.place(x=575, y=38)
 
 def obtenerPath():
     global RUTA
@@ -76,17 +83,21 @@ def obtenerPath():
 cambiarRutaBoton = ttk.Button(root, image=imagenCambiarRuta, command = obtenerPath)
 cambiarRutaBoton.place(x=660, y=31)
 
-# entry.config(state=tk.NORMAL)  MIENTRAS NO SE COMPLETE LA DESCARGA NO LE DEJO VOLVER A USARLA
+#Menu
+menuRaton = Menu(root, tearoff=False)
+menuRaton.add_command(label="Pegar")
+
+def mostrarMenu(e):
+    w = e.widget
+    menuRaton.entryconfigure("Pegar", command = lambda: w.event_generate("<<Paste>>"))
+    menuRaton.tk_popup(e.x_root, e.y_root)
+
+root.bind("<Button-3>", mostrarMenu)
+
+# --------------------------------------------------------------------------
 
 def main():
-    x_coord = int((SCREEN_WIDTH/2) - (WIDTH/2))
-    y_coord = int((SCREEN_HEIGHT/2) - (HEIGHT/2))
-    root.geometry("{}x{}+{}+{}".format(WIDTH, HEIGHT, x_coord, y_coord))
     root.mainloop()
-
-    url = entrada.get()
-    print(ruta)
-
 
 if __name__ == "__main__":
     main()
